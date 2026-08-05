8ba7814bed7dc305851a67f3ec5ad8ac3905a089
x-LazySpace-x <filipjarlestam@hotmail.com>
2026-08-05 02:05:54 +0200

    initial commit

---
+++ src/main/java/net/lazyspace/firstmod/FirstMod.java
@@ -0,0 +1,21 @@
+package net.lazyspace.firstmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import net.minecraft.util.Identifier;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FirstMod implements ModInitializer {
+	public static final String MOD_ID = "firstmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+
+	public static Identifier id(String path) {
+		return Identifier.of(MOD_ID, path);
+	}
+}
